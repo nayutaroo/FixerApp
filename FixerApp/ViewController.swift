0efcfd6a4348b6cca84011745004c86ba3f11df3
Tech-Nayuta <ketanayutawork@gmail.com>
2020-12-25 16:58:15 +0900

    Initial Commit

---
+++ FixerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FixerApp
+//
+//  Created by 化田晃平 on R 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FixerAppTests/FixerAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  FixerAppTests.swift
+//  FixerAppTests
+//
+//  Created by 化田晃平 on R 2/12/25.
+//
+
+import XCTest
+@testable import FixerApp
+
+class FixerAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
